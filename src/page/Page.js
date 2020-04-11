1c9b715d4b00b63156b310bd881c0f77513eb5c2
Jonjo McKay <jonjo@jonjomckay.com>
2020-04-11 15:56:42 +0100

    Added loading bar

--- src/page/Page.js
+++ src/page/Page.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PageContainer from './PageContainer';
 import Outcomes from '../outcomes/Outcomes';
+import ProgressBar from 'react-progress-bar-plus';
+import { Layout } from 'antd';
 
 const Page = props => {
     const containers = props.containers
@@ -9,16 +11,21 @@ const Page = props => {
         .map(container => <PageContainer container={ container } key={ container.id } />);
 
     return (
-        <div>
-            { containers }
+        <Layout>
+            <ProgressBar autoIncrement percent={ props.isLoading ? 25 : 100 } spinner="right" />
 
-            <Outcomes />
-        </div>
+            <Layout.Content style={{ padding: '1.5rem' }}>
+                { containers }
+
+                <Outcomes />
+            </Layout.Content>
+        </Layout>
     );
 };
 
 const mapStateToProps = state => ({
-    containers: state.page.pageContainers
+    containers: state.page.pageContainers,
+    isLoading: state.state.isLoading
 });
 
 export default connect(mapStateToProps)(Page);
--- src/state/stateReducer.js
+++ src/state/stateReducer.js
@@ -4,15 +4,23 @@ import { invokeFlow } from '../actions';
 const initialState = {
     currentMapElementId: null,
     id: null,
+    isLoading: false,
     token: null
 };
 
 export default createReducer(initialState, {
+    [invokeFlow.pending]: (state, action) => {
+        return {
+            ...state,
+            isLoading: true
+        }
+    },
     [invokeFlow.fulfilled]: (state, action) => {
         return {
             ...state,
             currentMapElementId: action.payload.currentMapElementId,
             id: action.payload.stateId,
+            isLoading: false, // TODO: This doesn't work with multiple concurrent loading things
             token: action.payload.stateToken
         }
     }
