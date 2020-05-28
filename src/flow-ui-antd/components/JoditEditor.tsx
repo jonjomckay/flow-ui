a89022456cbf53e2bd6cec3104ae45b609447d43
Jonjo McKay <jonjo@jonjomckay.com>
2020-05-28 18:41:22 +0100

    Half-converted JoditEditor to Typescript

--- src/flow-ui-antd/components/JoditEditor.tsx
+++ src/flow-ui-antd/components/JoditEditor.tsx
@@ -1,17 +1,29 @@
 /* eslint-disable */
 import React, { forwardRef, useEffect, useLayoutEffect, useRef } from 'react';
-import Jodit from 'jodit';
+import { Jodit } from 'jodit';
 import 'jodit/build/jodit.min.css';
+
+interface Props {
+    config: {},
+    name?: string;
+
+    onBlur?(value: string): void;
+
+    onChange(value: string): void;
+
+    tabIndex?: string;
+    value: string;
+}
 
 // TODO: This depends on a specific commit in package.json until this is released: https://github.com/xdan/jodit/pull/364
 // This is based on a comment from https://github.com/jodit/jodit-react/issues/43 because otherwise rendering screws up on update
 const JoditEditor = forwardRef(
-    ({ value, config, onChange, onBlur, tabIndex, name }, ref) => {
-        const textArea = useRef(null);
+    ({ value, config, onChange, onBlur, tabIndex, name }: Props, ref) => {
+        const textArea = useRef<any>(null);
 
         useLayoutEffect(() => {
             if (ref) {
-                if (typeof ref === "function") {
+                if (typeof ref === 'function') {
                     ref(textArea.current);
                 } else {
                     ref.current = textArea.current;
@@ -20,11 +32,11 @@ const JoditEditor = forwardRef(
         });
 
         useEffect(() => {
-            const blurHandler = value => {
+            const blurHandler = (value: string) => {
                 onBlur && onBlur(value);
             };
 
-            const changeHandler = value => {
+            const changeHandler = (value: string) => {
                 onChange && onChange(value);
             };
 
@@ -32,10 +44,10 @@ const JoditEditor = forwardRef(
             textArea.current = Jodit.make(element, config);
             textArea.current.value = value;
 
-            textArea.current.events.on("blur", () =>
+            textArea.current.events.on('blur', () =>
                 blurHandler(textArea.current.value)
             );
-            textArea.current.events.on("change", () =>
+            textArea.current.events.on('change', () =>
                 changeHandler(textArea.current.value)
             );
 
