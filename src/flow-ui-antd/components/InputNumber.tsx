646e66a94ccd36c749dfdf1cb2610f852d47933a
Jonjo McKay <jonjo@jonjomckay.com>
2020-05-29 17:25:25 +0100

    Added prefix, suffix, step and precision support for number inputs

--- src/flow-ui-antd/components/InputNumber.tsx
+++ src/flow-ui-antd/components/InputNumber.tsx
@@ -4,13 +4,43 @@ import { PageComponentProps } from '../../flow-ui';
 import BaseFormItem from './BaseFormItem';
 
 export default function InputNumber({ component, onChange }: PageComponentProps): React.ReactElement<PageComponentProps> {
+    // If we're given a prefix or suffix, add them to the display value
+    const formatter = (value: string | number | undefined): string => {
+        if (component.attributes?.prefix) {
+            value = `${component.attributes.prefix}${value}`;
+        }
+
+        if (component.attributes?.suffix) {
+            value = `${value}${component.attributes.suffix}`;
+        }
+
+        return (value || '').toString();
+    };
+
+    // If we're given a prefix or suffix, we remove them from the display value before it becomes the input's value
+    const parser = (value: string | undefined): string => {
+        if (component.attributes?.prefix) {
+            value = value?.replace(component.attributes.prefix, '');
+        }
+
+        if (component.attributes?.suffix) {
+            value = value?.replace(component.attributes.suffix, '');
+        }
+
+        return (value || '').toString();
+    };
+
     const inputProps = {
         disabled: component.data.isEnabled === false,
+        formatter: formatter,
         id: component.id,
         placeholder: component.hintValue,
+        precision: Number.parseInt(component.attributes?.precision),
         required: component.data.isRequired,
         maxLength: component.maxSize,
+        parser: parser,
         readOnly: component.data.isEditable === false,
+        step: Number.parseFloat(component.attributes.step),
         width: component.width
     };
 
