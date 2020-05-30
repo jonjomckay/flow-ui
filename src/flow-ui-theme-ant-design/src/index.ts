0e8b624bd0aa7dd50cf069ed285767ce1d613f47
Jonjo McKay <jonjo@jonjomckay.com>
2020-05-30 13:40:50 +0100

    Made flow-ui a peer dependency, and removed dynamic imports for now

--- src/flow-ui-theme-ant-design/src/index.ts
+++ src/flow-ui-theme-ant-design/src/index.ts
@@ -1,35 +1,59 @@
-import * as React from 'react';
 import { ITheme } from '@jonjomckay/flow-ui';
+
+import Checkbox from './components/Checkbox';
+import Content from './components/Content';
+import Image from './components/Image';
+import Input from './components/Input';
+import List from './components/List';
+import Outcomes from './components/Outcomes';
+import Presentation from './components/Presentation';
+import Radio from './components/Radio';
+import SelectComponent from './components/SelectComponent';
+import Table from './components/Table';
+import Textarea from './components/Textarea';
+import Toggle from './components/Toggle';
+
+import GroupContainer from './containers/GroupContainer';
+import HorizontalContainer from './containers/HorizontalContainer';
+import InlineContainer from './containers/InlineContainer';
+import VerticalContainer from './containers/VerticalContainer';
+
+import Alert from './alert/Alert';
+import Loader from './loader/Loader';
+import Navigation from './navigation/Navigation';
+import Notifications from './notification/Notifications';
+import OutcomesList from './components/OutcomesList';
+import RootContainer from './containers/RootContainer';
 
 import './index.less';
 
 const AntDesign: ITheme = {
-    alertComponent: React.lazy(() => import(/* webpackChunkName: 'antd.alert' */ './alert/Alert')),
+    alertComponent: Alert,
     components: {
-        'CHECKBOX': React.lazy(() => import(/* webpackChunkName: 'antd.component.checkbox' */ './components/Checkbox')),
-        'CONTENT': React.lazy(() => import(/* webpackChunkName: 'antd.component.content' */ './components/Content')),
-        'IMAGE': React.lazy(() => import(/* webpackChunkName: 'antd.component.image' */ './components/Image')),
-        'INPUT': React.lazy(() => import(/* webpackChunkName: 'antd.component.input' */ './components/Input')),
-        'LIST': React.lazy(() => import(/* webpackChunkName: 'antd.component.list' */ './components/List')),
-        'OUTCOMES': React.lazy(() => import(/* webpackChunkName: 'antd.component.outcomes' */ './components/Outcomes')),
-        'PRESENTATION': React.lazy(() => import(/* webpackChunkName: 'antd.component.presentation' */ './components/Presentation')),
-        'RADIO': React.lazy(() => import(/* webpackChunkName: 'antd.component.radio' */ './components/Radio')),
-        'SELECT': React.lazy(() => import(/* webpackChunkName: 'antd.component.select' */ './components/SelectComponent')),
-        'TABLE': React.lazy(() => import(/* webpackChunkName: 'antd.component.table' */ './components/Table')),
-        'TEXTAREA': React.lazy(() => import(/* webpackChunkName: 'antd.component.textarea' */ './components/Textarea')),
-        'TOGGLE': React.lazy(() => import(/* webpackChunkName: 'antd.component.toggle' */ './components/Toggle')),
+        'CHECKBOX': Checkbox,
+        'CONTENT': Content,
+        'IMAGE': Image,
+        'INPUT': Input,
+        'LIST': List,
+        'OUTCOMES': Outcomes,
+        'PRESENTATION': Presentation,
+        'RADIO': Radio,
+        'SELECT': SelectComponent,
+        'TABLE': Table,
+        'TEXTAREA': Textarea,
+        'TOGGLE': Toggle,
     },
     containers: {
-        'GROUP': React.lazy(() => import(/* webpackChunkName: 'antd.container.group' */ './containers/GroupContainer')),
-        'HORIZONTAL_FLOW': React.lazy(() => import(/* webpackChunkName: 'antd.container.horizontal' */ './containers/HorizontalContainer')),
-        'INLINE_FLOW': React.lazy(() => import(/* webpackChunkName: 'antd.container.inline' */ './containers/InlineContainer')),
-        'VERTICAL_FLOW': React.lazy(() => import(/* webpackChunkName: 'antd.container.vertical' */ './containers/VerticalContainer'))
+        'GROUP': GroupContainer,
+        'HORIZONTAL_FLOW': HorizontalContainer,
+        'INLINE_FLOW': InlineContainer,
+        'VERTICAL_FLOW': VerticalContainer,
     },
-    loaderComponent: React.lazy(() => import(/* webpackChunkName: 'antd.loader' */ './loader/Loader')),
-    navigationComponent: React.lazy(() => import(/* webpackChunkName: 'antd.navigation' */ './navigation/Navigation')),
-    notificationsComponent: React.lazy(() => import(/* webpackChunkName: 'antd.notifications' */ './notification/Notifications')),
-    outcomesComponent: React.lazy(() => import(/* webpackChunkName: 'antd.outcomes' */ './components/OutcomesList')),
-    rootContainer: React.lazy(() => import(/* webpackChunkName: 'antd.container.root' */ './containers/RootContainer'))
+    loaderComponent: Loader,
+    navigationComponent: Navigation,
+    notificationsComponent: Notifications,
+    outcomesComponent: OutcomesList,
+    rootContainer: RootContainer
 }
 
 export default AntDesign;
