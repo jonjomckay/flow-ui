d4f72977df01b79881ddbe92110f02207fea90e9
Jonjo McKay <jonjo@jonjomckay.com>
2020-07-10 14:07:37 +0100

    v0.4.0

---
+++ src/flow-ui-theme-ant-design/src/components/InputDateTime.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { DatePicker, TimePicker } from 'antd';
+import { PageComponentProps } from '@jonjomckay/flow-ui';
+import BaseFormItem from './BaseFormItem';
+import { useState } from 'react';
+
+export default function InputDateTime({ component, onChange }: PageComponentProps): React.ReactElement<PageComponentProps> {
+    const [date, setDate] = useState();
+    const [time, setTime] = useState();
+
+    const inputProps = {
+        addonAfter: component.attributes?.suffix,
+        addonBefore: component.attributes?.prefix,
+        disabled: component.data.isEnabled === false,
+        id: component.id,
+        placeholder: component.hintValue,
+        required: component.data.isRequired,
+        maxLength: component.maxSize,
+        readOnly: component.data.isEditable === false,
+        style: {
+            width: `${component.size}em`
+        }
+    };
+
+    const onDateChange = (date: any, dateString: string) => {
+
+    };
+
+    const onTimeChange = (date: any, dateString: string) => {
+
+    };
+
+    console.log(component);
+
+    return (
+        <BaseFormItem component={ component }>
+            <div>
+                <DatePicker { ...inputProps } onChange={ (date, dateString) => onChange({ contentValue: dateString })} />
+                <TimePicker { ...inputProps } />
+            </div>
+        </BaseFormItem>
+    );
+}
