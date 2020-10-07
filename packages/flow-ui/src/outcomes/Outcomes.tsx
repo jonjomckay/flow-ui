ff806cd4cba59e76af8f5bac014071cf4672f42b
Jonjo McKay <jonjo@jonjomckay.com>
2020-10-07 21:35:54 +0100

    Cleaned up dead code from Outcomes

--- packages/flow-ui/src/outcomes/Outcomes.tsx
+++ packages/flow-ui/src/outcomes/Outcomes.tsx
@@ -1,24 +1,20 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { selectOutcome, SelectOutcomeProps } from '../actions';
-import { IOutcome } from '../types';
+import { selectOutcome } from '../actions';
 import { RootState } from '../store';
 import ITheme from '../theme/ITheme';
 import { OutcomesProps } from '../index';
 
 type Props = OutcomesProps & {
-    isLoading: boolean;
-    outcomes: IOutcome[];
     theme: ITheme;
-
-    selectOutcome(value: SelectOutcomeProps): void;
 }
 
 function Outcomes(props: Props): React.ReactElement<OutcomesProps> {
     // Create outcomes from all the outcome responses that aren't bound to a page object
     return React.createElement(props.theme.components.OUTCOMES, {
         ...props,
-        outcomes: props.outcomes.filter(outcome => outcome.pageObjectBindingId === null)
+        outcomes: props.outcomes.filter(outcome => outcome.pageObjectBindingId === null),
+        selectOutcome: props.selectOutcome
     });
 }
 
